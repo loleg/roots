e62c6812b9248c327f9d9e047dfe415533b2a1d2
Oleg Lavrovsky <oleg@utou.ch>
2014-07-02 17:13:54 +0200

    Borderless images

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '349b3300e70e2930d55953e0e2b9e1f2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '260168096207a3748bb7e6965723e072');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
