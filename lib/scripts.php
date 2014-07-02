fe5338c63e68ebd1f0da79227c13dc09fce52724
Oleg Lavrovsky <oleg@utou.ch>
2014-07-02 16:32:12 +0200

    Size of embedded videos

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '6ba7b284c605da7bb27633b881429c56');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '05c62f5aceb013df7645cc3ca69944a7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
